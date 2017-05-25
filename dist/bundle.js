f518e07a0ee64723a8cdda02415e5e385a88af2a
rnoelle <reidnoelle2@gmail.com>
2017-05-25 14:18:46 -0600

    added active navbar animation

--- dist/bundle.js
+++ dist/bundle.js
@@ -8,7 +8,7 @@ Vue.component('footer-elem', {
 Vue.component("Lightbox", VueLightbox.VueLightbox);
 
 Vue.component('gallery', {
-  template: "\n    <section id=\"gallery\">\n      <h2>Gallery</h2>\n      <div class=\"lightboxes\">\n        <lightbox src=\"../assets/bandj2.jpg\" caption=\"this is pic caption\" album=\"bandj\">\n          <img src=\"../assets/bandj2.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj1.jpg\" caption=\"this is pic caption\" album=\"bandj\">\n          <img src=\"../assets/bandj1.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj3.jpg\" caption=\"this is pic caption\" album=\"bandj\">\n          <img src=\"../assets/bandj3.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj4.jpg\" caption=\"this is pic caption\" album=\"bandj\">\n          <img src=\"../assets/bandj4.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj5.jpg\" caption=\"this is pic caption\" album=\"bandj\">\n          <img src=\"../assets/bandj5.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj6.jpg\" caption=\"this is pic caption\" album=\"bandj\">\n          <img src=\"../assets/bandj6.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj7.jpg\" caption=\"this is pic caption\" album=\"bandj\">\n          <img src=\"../assets/bandj7.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj8.jpg\" caption=\"this is pic caption\" album=\"bandj\">\n          <img src=\"../assets/bandj8.jpg\">\n        </lightbox>\n      </div>\n      <h3>Photos by <em><a href=\" https://www.facebook.com/jennsinesphotos/\" target=\"_blank\">Jennsine Erickson</a></em></h3>\n    </section>\n  "
+  template: "\n    <section id=\"gallery\">\n      <h2>Gallery</h2>\n      <div class=\"lightboxes\">\n        <lightbox src=\"../assets/bandj2.jpg\" caption=\"Photo by Jennsine Erickson\" album=\"bandj\">\n          <img src=\"../assets/bandj2.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj1.jpg\" caption=\"Photo by Jennsine Erickson\" album=\"bandj\">\n          <img src=\"../assets/bandj1.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj3.jpg\" caption=\"Photo by Jennsine Erickson\" album=\"bandj\">\n          <img src=\"../assets/bandj3.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj4.jpg\" caption=\"Photo by Jennsine Erickson\" album=\"bandj\">\n          <img src=\"../assets/bandj4.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj5.jpg\" caption=\"Photo by Jennsine Erickson\" album=\"bandj\">\n          <img src=\"../assets/bandj5.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj6.jpg\" caption=\"Photo by Jennsine Erickson\" album=\"bandj\">\n          <img src=\"../assets/bandj6.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj7.jpg\" caption=\"Photo by Jennsine Erickson\" album=\"bandj\">\n          <img src=\"../assets/bandj7.jpg\">\n        </lightbox>\n        <lightbox src=\"../assets/bandj8.jpg\" caption=\"Photo by Jennsine Erickson\" album=\"bandj\">\n          <img src=\"../assets/bandj8.jpg\">\n        </lightbox>\n      </div>\n      <h3>Photos by <em><a href=\" https://www.facebook.com/jennsinesphotos/\" target=\"_blank\">Jennsine Erickson</a></em></h3>\n    </section>\n  "
 });
 'use strict';
 
@@ -36,46 +36,58 @@ Vue.component('top', {
 Vue.component('wedding', {
   template: '\n    <section id="wedding" class="clearfix">\n        <div class="image"></div>\n        <div class="left">\n          <h2>The Wedding</h2>\n          <h4>The marriage will take place <em>30 June 2017</em>.</h4>\n          <h4>A reception will be held in their honor that night:</h4>\n          <h3>The Manderley Reception Center</h3>\n          <h3>1675 North Freedom Boulevard #10G</h3>\n          <h3><em>6-8pm</em></h3>\n          <h5> In consideration of family allergies, please do not wear scented products, such as lotions, cologne, perfume, or essential oils. Thank you!</h5>\n          <aside>\n            <h3><em>6-7pm</em> the couple will greet their guests.</h3>\n            <h3><em>7pm</em> the dancing will begin.</h3>\n            <h3><em>8pm</em> the couple will make their exit!</h3>\n          </aside>\n      </div>\n    </section>\n  '
 });
-// $(document).ready(function () {
-//     $(document).on("scroll", onScroll);
-//
-//     //smoothscroll
-//     $('a[href^="#"]').on('click', function (e) {
-//         e.preventDefault();
-//         $(document).off("scroll");
-//
-//         $('a').each(function () {
-//             $(this).removeClass('active');
-//         })
-//         $(this).addClass('active');
-//
-//         var target = this.hash,
-//             menu = target;
-//         $target = $(target);
-//         $('html, body').stop().animate({
-//             'scrollTop': $target.offset().top+2
-//         }, 500, 'swing', function () {
-//             window.location.hash = target;
-//             $(document).on("scroll", onScroll);
-//         });
-//     });
-// });
-//
-// function onScroll(event){
-//     var scrollPos = $(document).scrollTop();
-//     $('#menu-center a').each(function () {
-//         var currLink = $(this);
-//         var refElement = $(currLink.attr("href"));
-//         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-//             $('#menu-center ul li a').removeClass("active");
-//             currLink.addClass("active");
-//         }
-//         else{
-//             currLink.removeClass("active");
-//         }
-//     });
-// }
-"use strict";
+'use strict';
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        });
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target,
+            $target = $(document).find(target).first();
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top + 2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('#navbar-list a').each(function () {
+        var currLink = $(this);
+        if ($(currLink).attr("href")[0] == '#') {
+            var refElement = $(currLink.attr("href"));
+        } else {
+            return;
+        }
+
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#navbar-list ul li a').removeClass("active");
+            currLink.addClass("active");
+        } else {
+            currLink.removeClass("active");
+        }
+    });
+    if (scrollPos > 25) {
+        $('nav').addClass('opaque');
+        console.log('here!');
+    } else {
+        $('nav').removeClass('opaque');
+    }
+}
 'use strict';
 
 var app = new Vue({
