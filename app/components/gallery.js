33f1c2f5c46340cc0b4da5c78e2b616b6a40db9f
rnoelle <reidnoelle2@gmail.com>
2017-05-25 13:45:03 -0600

    fixed pic captions

--- app/components/gallery.js
+++ app/components/gallery.js
@@ -5,28 +5,28 @@ Vue.component('gallery', {
     <section id="gallery">
       <h2>Gallery</h2>
       <div class="lightboxes">
-        <lightbox src="../assets/bandj2.jpg" caption="this is pic caption" album="bandj">
+        <lightbox src="../assets/bandj2.jpg" caption="Photo by Jennsine Erickson" album="bandj">
           <img src="../assets/bandj2.jpg">
         </lightbox>
-        <lightbox src="../assets/bandj1.jpg" caption="this is pic caption" album="bandj">
+        <lightbox src="../assets/bandj1.jpg" caption="Photo by Jennsine Erickson" album="bandj">
           <img src="../assets/bandj1.jpg">
         </lightbox>
-        <lightbox src="../assets/bandj3.jpg" caption="this is pic caption" album="bandj">
+        <lightbox src="../assets/bandj3.jpg" caption="Photo by Jennsine Erickson" album="bandj">
           <img src="../assets/bandj3.jpg">
         </lightbox>
-        <lightbox src="../assets/bandj4.jpg" caption="this is pic caption" album="bandj">
+        <lightbox src="../assets/bandj4.jpg" caption="Photo by Jennsine Erickson" album="bandj">
           <img src="../assets/bandj4.jpg">
         </lightbox>
-        <lightbox src="../assets/bandj5.jpg" caption="this is pic caption" album="bandj">
+        <lightbox src="../assets/bandj5.jpg" caption="Photo by Jennsine Erickson" album="bandj">
           <img src="../assets/bandj5.jpg">
         </lightbox>
-        <lightbox src="../assets/bandj6.jpg" caption="this is pic caption" album="bandj">
+        <lightbox src="../assets/bandj6.jpg" caption="Photo by Jennsine Erickson" album="bandj">
           <img src="../assets/bandj6.jpg">
         </lightbox>
-        <lightbox src="../assets/bandj7.jpg" caption="this is pic caption" album="bandj">
+        <lightbox src="../assets/bandj7.jpg" caption="Photo by Jennsine Erickson" album="bandj">
           <img src="../assets/bandj7.jpg">
         </lightbox>
-        <lightbox src="../assets/bandj8.jpg" caption="this is pic caption" album="bandj">
+        <lightbox src="../assets/bandj8.jpg" caption="Photo by Jennsine Erickson" album="bandj">
           <img src="../assets/bandj8.jpg">
         </lightbox>
       </div>
