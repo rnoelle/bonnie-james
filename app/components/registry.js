bd5712b04f8cf4833ca3c0aa635f79e90fb57715
rnoelle <reidnoelle2@gmail.com>
2017-06-03 13:33:03 -0600

    updated registry with target

--- app/components/registry.js
+++ app/components/registry.js
@@ -3,12 +3,15 @@ Vue.component('registry', {
     <section id="registry">
       <h2>The couple is registered at:</h2>
       <div>
-        <a href='https://www.amazon.com/wedding/james-scott-bonnie-reid-provo-june-2017/registry/251XPRVK8MRVF' target="_blank">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Amazon.com-Logo.svg/2000px-Amazon.com-Logo.svg.png">
+        <a href='http://www.target.com/gift-registry/giftgiver?registryId=b991c274e19e4dda823e17df1e2bc760' target="_blank">
+          <img src="http://static.wixstatic.com/media/4a07ef_dd6b5375b3ab4811865ac03536efd67a~mv2.png">
         </a>
-        <a href="https://www.honeyfund.com/wedding/bonnieandjamesforever">
-          <img src="https://www.capolyeverafter.com/wp-content/uploads/2016/10/logo-honeyfund.png">
+        <a href="https://www.honeyfund.com/wedding/bonnieandjamesforever" target="_blank">
+        <img src="https://www.capolyeverafter.com/wp-content/uploads/2016/10/logo-honeyfund.png">
         The couple are in need of a washer and dryer - you can contribute here!
+      </a>
+        <a href='https://www.amazon.com/wedding/james-scott-bonnie-reid-provo-june-2017/registry/251XPRVK8MRVF' target="_blank">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Amazon.com-Logo.svg/2000px-Amazon.com-Logo.svg.png">
         </a>
       </div>
     </section>
--- dist/bundle.js
+++ dist/bundle.js
@@ -24,7 +24,7 @@ Vue.component('navbar', {
 'use strict';
 
 Vue.component('registry', {
-  template: '\n    <section id="registry">\n      <h2>The couple is registered at:</h2>\n      <div>\n        <a href=\'https://www.amazon.com/wedding/james-scott-bonnie-reid-provo-june-2017/registry/251XPRVK8MRVF\' target="_blank">\n          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Amazon.com-Logo.svg/2000px-Amazon.com-Logo.svg.png">\n        </a>\n        <a href="https://www.honeyfund.com/wedding/bonnieandjamesforever">\n          <img src="https://www.capolyeverafter.com/wp-content/uploads/2016/10/logo-honeyfund.png">\n        The couple are in need of a washer and dryer - you can contribute here!\n        </a>\n      </div>\n    </section>\n  '
+  template: '\n    <section id="registry">\n      <h2>The couple is registered at:</h2>\n      <div>\n        <a href=\'http://www.target.com/gift-registry/giftgiver?registryId=b991c274e19e4dda823e17df1e2bc760\' target="_blank">\n          <img src="http://static.wixstatic.com/media/4a07ef_dd6b5375b3ab4811865ac03536efd67a~mv2.png">\n        </a>\n        <a href="https://www.honeyfund.com/wedding/bonnieandjamesforever" target="_blank">\n        <img src="https://www.capolyeverafter.com/wp-content/uploads/2016/10/logo-honeyfund.png">\n        The couple are in need of a washer and dryer - you can contribute here!\n      </a>\n        <a href=\'https://www.amazon.com/wedding/james-scott-bonnie-reid-provo-june-2017/registry/251XPRVK8MRVF\' target="_blank">\n        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Amazon.com-Logo.svg/2000px-Amazon.com-Logo.svg.png">\n        </a>\n      </div>\n    </section>\n  '
 });
 'use strict';
 
